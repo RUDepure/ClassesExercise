2065de4c58a03536cc5e27763907d67c2d574c68
RUDepure <alexrum92@gmail.com>
2021-10-08 10:01:30 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car firstCar = new Car();
+
+            firstCar.Make = "Honda";
+            firstCar.Model = "Civic";
+            firstCar.Year = 2016;
+
+            Console.WriteLine($"My first car is a {firstCar.Make} {firstCar.Model} {firstCar.Year}");
+        }
+    }
+}
